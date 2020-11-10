bfb87f2ea19051ffb119613d44e34e5ff90f5be0
Chubi Adejoh <michaeladejoh01@gmail.com>
2020-11-10 18:46:41 +0400

    created lessons array and added lessons objects

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessons = [
+    {
+        id: 1,
+        Subject: "Math",
+        Location: "London",
+        Price: "300",
+        Spaces: 5,
+        image: ""
+    }
+    ,{
+        id: 2,
+        Subject: "English",
+        Location: "Manchester",
+        Price: "250",
+        Spaces: 10,
+        image: ""
+    }
+    ,{
+        id: 3,
+        Subject: "Chemistry",
+        Location: "Tottenham",
+        Price: "900",
+        Spaces: 15,
+        image: ""
+    }
+    ,{
+        id: 4,
+        Subject: "Physics",
+        Location: "Essex",
+        Price: "200",
+        Spaces: 6,
+        image: ""
+    }
+    ,{
+        id: 5,
+        Subject: "Calculus",
+        Location: "Sussex",
+        Price: "150",
+        Spaces: 12,
+        image: ""
+    }
+    ,{
+        id: 6,
+        Subject: "Statistics",
+        Location: "Chelsea",
+        Price: "100",
+        Spaces: 20,
+        image: ""
+    }
+    ,{
+        id: 7,
+        Subject: "Linguistics",
+        Location: "Dublin",
+        Price: "900",
+        Spaces: 13,
+        image: ""
+    }
+    ,{
+        id: 8,
+        Subject: "Astronomy",
+        Location: "Edinburgh",
+        Price: "1000",
+        Spaces: 3,
+        image: ""
+    }
+    ,{
+        id: 9,
+        Subject: "Sociology",
+        Location: "Brighton",
+        Price: "150",
+        Spaces: 5,
+        image: ""
+    }
+    ,{
+        id: 10,
+        Subject: "Algebra",
+        Location: "Liverpool",
+        Price: "450",
+        Spaces: 12,
+        image: ""
+    }
+]
